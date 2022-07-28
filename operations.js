355e5ca5d512ecf83518d290f918fef77c2febb9
Ali-consciousroad <ali_dindar@live.be>
2022-07-28 12:42:36 +0200

    Node MongoDB example 2

---
+++ operations.js
@@ -0,0 +1,42 @@
+// Node module of CRUD DB operations
+const assert = require("assert");
+
+// Create operation
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.insert(document, (err, result) => {
+        assert.equal(err, null);
+        console.log("Inserted " + result.result.n + 
+            " documents into the collection " + collection);
+        callback(result);
+    });
+};
+
+// Read operation
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.find({}).toArray((err, docs) => {
+        assert.equal(err, null);
+        callback(docs);
+    });
+};
+
+// Delete operation
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.deleteOne(document, (err, result) => {
+        assert.equal(err, null);
+        console.log("Removed the document ", document);
+        callback(result);
+    });
+};
+
+// Update operation
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.updateOne(document, { $set: update }, null, (err, result) => {
+        assert.equal(err, null);
+        console.log("Updated the document with ", update);
+        callback(result);
+    });
+};
